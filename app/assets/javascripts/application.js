8aa60381e018f43b8887144ec73195ab9804da0d
Brad Kingon <bkingon@gmail.com>
2016-09-04 00:22:46 +0200

    basic foundation layout

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,8 +1,11 @@
 //= require jquery
 //= require jquery_ujs
+//= require foundation
 //= require underscore
 //= require gmaps/google
 //= require maps4rails.min
 //= require gmaps_google
 
 // $(function(){ $(document).foundation() })
+
+$(function(){ $(document).foundation(); });
