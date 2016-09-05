f1841b989b0322b05e658ae07b642e1f2d23efb0
Brad Kingon <bkingon@gmail.com>
2016-09-05 22:30:37 +0200

    refactored

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,12 +1,28 @@
 class DashboardController < ApplicationController
   def index
-    json_earthquakes = EarthquakeQuery.new(filter_params).call
+    response = EarthquakeQuery.new(filter_params).call
+    json_earthquakes = evaluate_response(response)
 
     @filtered_params = {starttime: start_time.to_date.strftime("%Y-%m-%d"), endtime: end_time.to_date.strftime("%Y-%m-%d"), minmagnitude: min_magnitude}
     @hash = MapMarkers.new(json_earthquakes).call
+    flash[:alert] = @error_message
   end
 
   private
+  def evaluate_response(response)
+    if response.status != 200
+      if response.body.first(1000).include?('exceeds search limit')
+        @error_message = 'Search parameters exceed 20,000 results. Please modify your search parameters.'
+      else
+        @error_message = 'Something went wrong with your search, please try again.'
+      end
+
+      params.delete(:filtered_params)
+      response = EarthquakeQuery.new(filter_params).call
+    end
+
+    JSON.parse(response.body)['features']
+  end
 
   def filter_params
     {format: 'geojson', starttime: start_time, endtime: end_time, minmagnitude: min_magnitude}
--- app/services/earthquake_query.rb
+++ app/services/earthquake_query.rb
@@ -5,15 +5,7 @@ class EarthquakeQuery
 
   def call
     connection = EarthquakeApi.new.call
-    response = connection.get '/fdsnws/event/1/query', query_params
-
-    if response.body.first(1000).include?('exceeds search limit')
-      query_params.delete(:filtered_params)
-      response = connection.get '/fdsnws/event/1/query', query_params
-      @error_message = 'Search parameters exceed 20,000 results. Please modify your search parameters.'
-    end
-
-    JSON.parse(response.body)['features']
+    connection.get '/fdsnws/event/1/query', query_params
   end
 
   private
