52963a73d5ee97209f86b441ecdfdc3c2b2632d2
Brad Kingon <bkingon@gmail.com>
2016-09-05 23:08:26 +0200

    added tests to check error case

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -34,5 +34,31 @@ RSpec.describe DashboardController, type: :controller do
       expect(assigns(:filtered_params)[:minmagnitude]).to eq 0
       expect(response.status).to eq(200)
     end
+
+    it 'sets the error message if the query returns a bad response' do
+      allow_any_instance_of(Faraday::Response).to receive(:status).and_return 400
+      VCR.use_cassette 'bad_response_query' do
+        get :index, params: { filtered_params: { starttime: '2016-08-01', endtime: '2016-09-03', minmagnitude: 7 } }
+      end
+
+      expect(flash[:alert]).to eq 'Something went wrong with your search, please try again.'
+      expect(response).to render_template :index
+      expect(assigns(:hash).count).to eq 231
+      expect(response.status).to eq(200)
+    end
+
+    it 'sets the error message if the query count exceeds 20,000' do
+      allow_any_instance_of(Faraday::Response).to receive(:status).and_return 400
+      allow_any_instance_of(String).to receive_message_chain(:first, :include?).and_return(true)
+
+      VCR.use_cassette 'exceeds_query_limit' do
+        get :index, params: { filtered_params: { starttime: '2016-08-01', endtime: '2016-09-03', minmagnitude: 7 } }
+      end
+
+      expect(flash[:alert]).to eq 'Search parameters exceed 20,000 results. Please modify your search parameters.'
+      expect(response).to render_template :index
+      expect(assigns(:hash).count).to eq 231
+      expect(response.status).to eq(200)
+    end
   end
 end
