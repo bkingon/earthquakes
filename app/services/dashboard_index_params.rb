964315c834b16f87890948a80271ccf73ed620ea
Brad Kingon <bkingon@gmail.com>
2016-09-06 13:09:28 +0200

    created DashboardIndexParams class for earthquake query

---
+++ app/services/dashboard_index_params.rb
@@ -0,0 +1,47 @@
+class DashboardIndexParams
+  def self.parse(params)
+    if has_filtered_params?(params)
+      return DashboardIndexParams.new(params)
+    end
+
+    return NullDashboardIndexParams.new
+  end
+
+  def initialize(params)
+    @params = params
+  end
+
+  def start_time
+    params['filtered_params']['starttime']
+  end
+
+  def end_time
+    params['filtered_params']['endtime']
+  end
+
+  def min_magnitude
+    params['filtered_params']['minmagnitude'].to_i
+  end
+
+  private
+  attr_reader :params
+
+  private_class_method
+  def self.has_filtered_params?(params)
+    params['filtered_params'].present?
+  end
+end
+
+class NullDashboardIndexParams
+  def start_time
+    Date.yesterday.to_s
+  end
+
+  def end_time
+    Date.current.to_s
+  end
+
+  def min_magnitude
+    0
+  end
+end
