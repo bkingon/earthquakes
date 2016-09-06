c4e7c2f5fd97a31d7983a4e15a5a6b40cd6f7f06
Brad Kingon <bkingon@gmail.com>
2016-09-06 13:09:51 +0200

    refactored EarthquakeApi to use simpledelegator

--- app/services/earthquake_api.rb
+++ app/services/earthquake_api.rb
@@ -1,5 +1,10 @@
-class EarthquakeApi
-  def call
+class EarthquakeApi < SimpleDelegator
+  def initialize(client = nil)
+    super(client || default_api_client)
+  end
+
+  private
+  def default_api_client
     Faraday.new(:url => 'http://earthquake.usgs.gov') do |faraday|
       faraday.request  :url_encoded             # form-encode POST params
       faraday.response :logger                  # log requests to STDOUT
